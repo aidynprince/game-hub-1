a414f7d19b7fd51487c928e4db24b11680b257ab
aidynprince <princeadjei231@outlook.com>
2025-08-06 15:14:28 +0000

    setup routing

---
+++ src/routes/router.tsx
@@ -0,0 +1,16 @@
+import { createBrowserRouter } from "react-router-dom";
+import GameDetailPage from "../components/GameDetailPage";
+import HomePage from "../pages/HomePage";
+import Layout from "../pages/layout";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Layout></Layout>,
+        children: [
+            { index: true, element: <HomePage></HomePage> },
+            { path: "games/:id", element: <GameDetailPage></GameDetailPage> },
+        ],
+    },
+]);
+export default router;
